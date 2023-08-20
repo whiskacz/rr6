4c318dd48a1de95a351b687fd14c76e18ae021e2
whiskacz <pawelblasiak1985@gmail.com>
2023-08-20 22:17:36 +0200

    useReduce vs Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import  ReactDOM  from 'react-dom/client';
+
+function RR6 () {
+
+    // @@@@@@@@@@@@@@@@ USE STATE
+
+    // const [count,setCount] = useState(0)
+
+    // const handleIncrement = () => {
+    //     setCount(count + 1)
+    // }
+
+    // const handleDecrement = () => {
+    //     setCount(count - 1)
+    // }
+
+    // return (
+    //     <div>
+    //         <div>{`COUNT IS ${count}`}</div>
+    //         <button onClick={handleIncrement}>+</button>
+    //         <button onClick={handleDecrement}>-</button>
+    //     </div>
+    // )
+
+    // @@@@@@@@@@@@@@ USE REDUCE
+
+
+
+
+
+
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RR6 />)
