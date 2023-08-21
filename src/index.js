de8d61f74cbba4f10e85fdd403b763a1908c119c
whiskacz <pawelblasiak1985@gmail.com>
2023-08-21 17:51:53 +0200

    all options checked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import  ReactDOM  from 'react-dom/client';
 
 function RR6 () {
@@ -15,20 +15,58 @@ function RR6 () {
     //     setCount(count - 1)
     // }
 
+    
     // return (
-    //     <div>
-    //         <div>{`COUNT IS ${count}`}</div>
-    //         <button onClick={handleIncrement}>+</button>
-    //         <button onClick={handleDecrement}>-</button>
-    //     </div>
+    // <div>
+    //     <div>{`COUNT IS ${count}`}</div>
+    //     <button onClick={handleIncrement}>+</button>
+    //     <button onClick={handleDecrement}>-</button>
+    // </div>
     // )
+    
 
     // @@@@@@@@@@@@@@ USE REDUCE
+// const [state,dispatch] = useReducer(reducer, {count:22})
+
+// function reducer (state,action) {
+//     if(action.type === "increment_age"){
+//         return {
+//             count: state.count +1
+//         };
+//     }
+//     else if (action.type === "decrement_age"){
+//         return {
+//             count: state.count -1
+//         };
+//     }
+    
+//     throw Error('Unknown action')
+// }
 
 
+//@@@@@@@@@@@@@@@@@@@@@@@@ USE REDUCE ON SWITCH @@@@@@@@@@@@@@@@@@@@@
 
 
+const [state,dispatch] = useReducer(reducer, {count:22})
 
+function reducer (state,action) {
+    switch(action.type) {
+        case "increment_age": 
+        return {count : state.count +1};
+        case "decrement_age":
+        return {count : state.count -1};
+    default:
+    throw Error('Unknown action')
+}
+}
+
+return (
+    <div>
+        <div>{`COUNT IS ${state.count}`}</div>
+        <button onClick={() => {dispatch({type: 'increment_age'})}}>+</button>
+        <button onClick={() => {dispatch({type: 'decrement_age'})}}>-</button>
+    </div>
+)
 
 
 }
